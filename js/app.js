bbf688430eabb82227aa4c1b5e2d2341bdc844a5
kholliday88 <kholliday88@gmail.com>
2016-05-16 16:26:51 -0400

    All main functionality is complete!

--- js/app.js
+++ js/app.js
@@ -31,7 +31,11 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('MainViewController', ['$scope', '$http', 'FilmService', '$location', function($scope, $http, FilmService, $location) {
   $scope.nowPlaying = FilmService.getNowPlaying();
   $scope.upcoming = FilmService.getUpcoming();
-
+  $scope.picks = [1,2,3];
+  Promise.all([FilmService.getFilmById(270303),FilmService.getFilmById(205596),FilmService.getFilmById(37799),FilmService.getFilmById(264644),FilmService.getFilmById(295699),]).then(function(picks) {
+    $scope.picks = picks;
+    $scope.$apply();
+  });
   $scope.getMovie = function(search) {
     var title = $location.path('/film/' + search.id);
   };
@@ -41,24 +45,23 @@ app.controller('MainViewController', ['$scope', '$http', 'FilmService', '$locati
 app.controller('SearchViewController', ['$scope', '$http', 'FilmService', '$location', '$routeParams', function($scope, $http, FilmService, $location, $routeParams) {
   FilmService.searchBy($routeParams.query).then(function(searchedMovies) {
     $scope.searched = searchedMovies;
-      $scope.queryOne = $routeParams.query;
-      
-      $scope.getMovie = function(movie) {
-        var title = $location.path('/film/' + movie.id);
-      };
-        // let year = searchedMovies.release_date;
-        // $scope.yearResponse = moment(year).format('YYYY');
-      });
+    $scope.queryOne = $routeParams.query;
+    $scope.getMovie = function(movie) {
+      var title = $location.path('/film/' + movie.id);
+    };
+  });
 }]);
 
 //Film View Controller
-app.controller('FilmViewController', ['$scope', '$http', 'FilmService', '$routeParams', function($scope, $http, FilmService, $routeParams) {
+app.controller('FilmViewController', ['$scope', '$http', 'FilmService', '$routeParams', '$location', function($scope, $http, FilmService, $routeParams, $location) {
   FilmService.getFilmById($routeParams.filmId).then(function(stuff) {
     $scope.movie = stuff;
+    console.log(stuff);
     let poster = stuff.poster_path;
     $scope.poster = "http://image.tmdb.org/t/p/w500" + poster;
     let year = stuff.release_date;
     $scope.yearResponse = moment(year).format('YYYY');
+    $scope.director = stuff.credits.crew[0].name;
     $scope.actor1 = stuff.credits.cast[0].name;
     $scope.actor2 = stuff.credits.cast[1].name;
     $scope.actor3 = stuff.credits.cast[2].name;
@@ -66,12 +69,11 @@ app.controller('FilmViewController', ['$scope', '$http', 'FilmService', '$routeP
     $scope.trailer = 'https://www.youtube.com/watch?v=' + stuff.trailers.youtube[0].source;
   });
   FilmService.getSimilar($routeParams.filmId).then(function(stuffTwo) {
-    $scope.movieTwo = stuffTwo;
-    $scope.similar1 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[0].poster_path;
-    $scope.similar2 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[1].poster_path;
-    $scope.similar3 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[2].poster_path;
-    $scope.similar4 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[3].poster_path;
-    $scope.similar5 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[4].poster_path;
+    $scope.movieTwo = stuffTwo.results;
+    console.log(stuffTwo);
+    $scope.getMovie = function(movie) {
+      var title = $location.path('/film/' + movie.id);
+    };
   });
 }]);
 
