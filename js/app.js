bba0f97ce99ec1bd991dbcb0e68882cb3505ce66
kholliday88 <kholliday88@gmail.com>
2016-05-16 00:03:18 -0400

    Midnight

--- js/app.js
+++ js/app.js
@@ -22,10 +22,6 @@ app.config(['$routeProvider', function ($routeProvider){
       controller: 'FilmViewController',
       templateUrl: 'sections/film.html',
     })
-    .when('/buzz', {
-      controller: 'BuzzViewController',
-      templateUrl: 'sections/buzz.html',
-    })
     .otherwise({
       redirectTo: '/main',
     });
@@ -34,20 +30,24 @@ app.config(['$routeProvider', function ($routeProvider){
 //Main Feed View controller
 app.controller('MainViewController', ['$scope', '$http', 'FilmService', '$location', function ($scope, $http, FilmService, $location) {
   $scope.nowPlaying = FilmService.getNowPlaying();
+  $scope.upcoming = FilmService.getUpcoming();
 
-  $scope.getMovie = function(movie) {
-      var title = $location.path('/film/' + movie.id);
+  $scope.getMovie = function(search) {
+      var title = $location.path('/film/' + search.id);
   };
 }
-
 ]);
+
 //Search View Controller
-app.controller('SearchViewController', ['$scope', '$http', 'FilmService', '$routeParams', function ($scope, $http, FilmService, $routeParams) {
+app.controller('SearchViewController', ['$scope', '$http', 'FilmService', '$location', '$routeParams', function ($scope, $http, FilmService, $location, $routeParams) {
   FilmService.searchBy($routeParams.query).then(function(searchedMovies) {
     $scope.searched = searchedMovies;
-    let year = searchedMovies.release_date;
-    $scope.yearResponse = moment(year).format('YYYY');
-    
+    $scope.queryOne = $routeParams.query;
+    $scope.getMovie = function(movie) {
+      var title = $location.path('/film/' + movie.id);
+    };
+    // let year = searchedMovies.release_date;
+    // $scope.yearResponse = moment(year).format('YYYY');
   });
 }
 ]);
@@ -56,7 +56,6 @@ app.controller('SearchViewController', ['$scope', '$http', 'FilmService', '$rout
 app.controller('FilmViewController', ['$scope', '$http', 'FilmService', '$routeParams', function ($scope, $http, FilmService, $routeParams) {
   FilmService.getFilmById($routeParams.filmId).then(function(stuff) {
     $scope.movie = stuff;
-    console.log(stuff);
     let poster = stuff.poster_path;
     $scope.poster = "http://image.tmdb.org/t/p/w500" + poster;
     let year = stuff.release_date;
@@ -66,12 +65,14 @@ app.controller('FilmViewController', ['$scope', '$http', 'FilmService', '$routeP
     $scope.actor3 = stuff.credits.cast[2].name;
     $scope.trailer = 'https://www.youtube.com/watch?v=' + stuff.trailers.youtube[0].source;
     });
-}
-]);
-
-//Buzz View Controller
-app.controller('BuzzViewController', ['$scope', '$http', 'FilmService', function ($scope, $http, FilmService) {
-
+    FilmService.getSimilar($routeParams.filmId).then(function(stuffTwo){
+      $scope.movieTwo = stuffTwo;
+      $scope.similar1 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[0].poster_path;
+      $scope.similar2 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[1].poster_path;
+      $scope.similar3 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[2].poster_path;
+      $scope.similar4 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[3].poster_path;
+      $scope.similar5 = 'http://image.tmdb.org/t/p/w300/' + stuffTwo.results[4].poster_path;
+    });
 }
 ]);
 
--- js/service.js
+++ js/service.js
@@ -6,6 +6,7 @@ module.exports = (function(){
   let movie = [];
   let nowPlaying = [];
   let newSearch = [];
+  let upcoming =[];
 
   service.factory('FilmService', function ($http) {
     //let all my variables go here
@@ -21,9 +22,24 @@ module.exports = (function(){
       angular.copy(response.data.results, nowPlaying);
       return response;
     });
+    $http({
+      method: 'get',
+      url: 'http://api.themoviedb.org/3/movie/upcoming?api_key=3f5dcd7179e2ff7c3180ca67d78b3936'
+    })
+    .then(function(response){
+      for (let i = 0; i < response.data.results.length; i++) {
+        var poster = response.data.results[i].poster_path;
+        response.data.results[i].fullPoster = "http://image.tmdb.org/t/p/w300" + poster;
+      }
+      angular.copy(response.data.results, upcoming);
+      return response;
+    });
     return {
       getNowPlaying: function() {
         return nowPlaying;
+      },
+      getUpcoming: function() {
+        return upcoming;
       },
       getFilmById: function(id) {
         return $http({
@@ -32,6 +48,15 @@ module.exports = (function(){
         })
         .then(function(result){
           return result.data;
+        });
+      },
+      getSimilar: function(id) {
+        return $http({
+          method: 'get',
+          url: 'https://api.themoviedb.org/3/movie/' + id + '/similar?api_key=3f5dcd7179e2ff7c3180ca67d78b3936&append_to_response=trailers,credits'
+        })
+        .then(function(similarResult){
+          return similarResult.data;
         });
       },
       searchBy: function(query){
